20b2780b9b46736c0161e626d03226675349f26e
Valentyn N Sichkar <34546286+sichkar-valentyn@users.noreply.github.com>
2018-01-26 12:36:19 +0300

    Update Analysis_of_Characteristics_for_Direct_Current_Motor_in_Matlab.m

--- Analysis_of_Characteristics_for_Direct_Current_Motor_in_Matlab.m
+++ Analysis_of_Characteristics_for_Direct_Current_Motor_in_Matlab.m
@@ -10,8 +10,8 @@
 % [1] Valentyn N Sichkar. Analysis of Characteristics for Direct Current Motor in Matlab // GitHub platform [Electronic resource]. URL: https://github.com/sichkar-valentyn/Analysis_of_Characteristics_for_Direct_Current_Motor_in_Matlab (date of access: XX.XX.XXXX)
 
 
-%Simulation of Linear Control System using Functions From Control System Toolbox
-%Analysis of Characteristics for Direct Current Motor
+%Simulation of Linear Control System using Functions From Control System Toolbox 
+%Analysis of Characteristics for Direct Current Motor 
 close all;
 
 %% Motor Properties
@@ -25,21 +25,21 @@ Ta = La/Ra;     % Inductance Resistance Ratio
 Ke=Um_max/Wmax; % Electromotive force constant  Units: volts / (rad/sec)
 Km=Ke;          % Torque constant               Units: Nm / (Work)^2
 
-%% Create transfer functions for each inertia
+%% Create transfer functions for each inertia 
 i = 1;
 for J = 0.5E-6 : 0.5E-6 : 0.4E-5 
     %Store transfer functions
     Tm=(J*Ra)/(Ke*Km);
     theTFs(i) = tf(1/Ke, [Tm*Ta Tm 1]);
     
-    %Store legend entries
+    %Store legend entries 
     theLegend(i) = {['J=' num2str(J)]};
     i = i + 1;
 end
 L = size(theTFs);
 L = L(2); % gets second numeber, which is length of the array
 
-%% Nyquist
+%% Nyquist 
 figure(1); hold;
 P = nyquistoptions;
 P.FreqUnits = 'Hz';
@@ -48,7 +48,7 @@ for i = 1:L
 end
 legend(theLegend);
 
-%% Bode
+%% Bode 
 figure(2); hold;
 P = bodeoptions;
 P.FreqUnits = 'Hz';
@@ -57,14 +57,14 @@ for i = 1:L
 end
 legend(theLegend);
     
-%% Step
+%% Step 
 figure(3); hold;
 for i = 1:L
      step(theTFs(i), 0:0.001:0.15); grid on
 end
 legend(theLegend);
 
-%% Impulse
+%% Impulse 
 figure(4); hold;
 for i = 1:L
 impulse(theTFs(i), 0:0.001:0.15); grid on
